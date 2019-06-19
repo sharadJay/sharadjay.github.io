776f8fbae9b9b74feb2679050ac2bd2eefa9db7f
sharad jain <shjain@expedia.com>
2019-06-19 19:29:56 +0530

    first set of changes

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,31 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  siteTitle: "FoodForCode - Blog with coding recipes",
+  shortSiteTitle: "Food For Code",
+  siteDescription: "FoodForCode is a personal blog",
+  siteUrl: "https://sharadjay.github.io",
   pathPrefix: "",
-  siteImage: "preview.jpg",
+  siteImage: "",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Sharad Jain",
   // info
-  infoTitle: "greg lobinski",
+  infoTitle: "Sharad Jain",
   infoTitleNote: "personal blog",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
+  manifestName: "FoodForCode - Blog with coding recipes",
   manifestShortName: "PersonalBlog", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "sharadj1591@gmail.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "https://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/sharadjay" }
+    // { name: "twitter", url: "https://twitter.com/greglobinski" },
+    // { name: "facebook", url: "https://facebook.com/greglobinski" }
   ]
 };
